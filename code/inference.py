929e538671d142af8a9cb979110b61b07af7b099
AxelBegue <119434974+AxelBegue@users.noreply.github.com>
2023-05-31 11:33:21 +0200

    Delete inference.py

--- code/inference.py
+++
@@ -1,5 +0,0 @@
-import os
-
-command = 'python ./code/test.py --dataset DrivingStereo --data_path ./datasets/ --ckpt_dir ./ckpt/best_model.ckpt --do_evaluate --max_depth 80.0 --max_depth_eval 80.0'
-
-os.system(command)
